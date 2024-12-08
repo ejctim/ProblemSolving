3964fbe7f04a483d06d208903b0c0e053c609cb4
Tim Johansson <tim@ejc.nu>
2024-12-08 01:50:26 +0100

    Some stuff added

--- Advent of code/4/4.scala
+++ Advent of code/4/4.scala
@@ -3,13 +3,19 @@ val lines = scala.io.Source.fromFile("4 input.txt").mkString
 @main
 def main(): Unit =
   val lineSeq = lines.split("\n").toSeq
-  val lineSlides = lines.sliding(4)
-  var sum = 0
-  lineSlides.foreach(str =>
-    str match
-      case "XMAS" => sum += 1
-      case "SMAS" => sum += 1
-      case _ =>
+  val charMatix = lineSeq.map(line => line.trim.split("").toSeq)
+  val x = 0
+  val y = 0
+  charMatix.foreach(s =>
+    s.foreach(char =>
+      if char == "X" then
+        lookAround(x,y)
+      y += 1
+    )
+    x += 1
   )
+
+  def lookAround(x: Int, y: Int): Unit =
+    if charMatix(x)(y)
 
   println(sum)
